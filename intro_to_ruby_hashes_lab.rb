180899a69f045b34542f448e018802ae15231f0a
rational-architecture-1641 <jarmah241@gmail.com>
2020-09-30 20:58:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,6 +11,20 @@ def monopoly_with_second_tier
   # complexity.
   {:railroads => {
     :pieces => 4,
+    :rent_in_dollars => {},
+    :names => {}
+  }}
+
+end
+
+def monopoly_with_third_tier
+  # When you start writing the implementation for this method, copy the Hash
+  # you built in the previous method. Run the tests. They will fail, but
+  # they'll guide you in how to modify what you just did to have more
+  # complexity.
+  
+  {:railroads => {
+    :pieces => 4,
     :rent_in_dollars => {
       :one_pieces_owned => 25,
       :two_pieces_owned => 50,
@@ -23,19 +37,6 @@ def monopoly_with_second_tier
       :b_and_o_railroad => {},
       :shortline_raildroad => {}
     }
-  }}
-
-end
-
-def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
-  {:railroads => {
-    :pieces => 4,
-    :rent_in_dollars => {},
-    :names => {}
   }}
 
 end
